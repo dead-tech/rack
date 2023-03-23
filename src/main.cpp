e05a82593804b0f8427657a634dbe77878863922
dead-tech <dev.dead.tech@gmail.com>
2023-03-23 19:12:59 +0100

    :sparkles: Read input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#define FMT_HEADER_ONLY
 #include <argparse/argparse.hpp>
+#include <dtslib/filesystem.hpp>
+#include <fmt/printf.h>
 
 
 int main(const int argc, const char** argv) {
@@ -9,8 +12,19 @@ int main(const int argc, const char** argv) {
       .set_flags(argparse::ArgFlags::REQUIRED)
       .set_help("path to file to compile");
 
-    const auto args = parser.parse_args();
+    const auto        args = parser.parse_args();
     const std::string file = args.at("file").as<std::string>();
+
+    const auto file_contents = dts::read_file<std::string>(file);
+
+    if (!file_contents.has_value()) {
+        fmt::print(
+          "[ERROR]: error occurred while reading file {}: {}\n",
+          file,
+          dts::filesystem_error(file_contents.error())
+        );
+        return 1;
+    }
 
     return 0;
 }
