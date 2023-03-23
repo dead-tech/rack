0267b21d9484982d96c3e516c08041fce4437e7c
dead-tech <dev.dead.tech@gmail.com>
2023-03-23 18:32:29 +0100

    :sparkles: Hook up ArgumentParser to capture input file from cmdline

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
-int main() {
+#include <argparse/argparse.hpp>
+
+
+int main(const int argc, const char** argv) {
+    argparse::ArgumentParser parser(argc, argv);
+    parser.add_argument("file")
+      .set_nargs(1)
+      .set_type(argparse::ArgTypes::STRING)
+      .set_flags(argparse::ArgFlags::REQUIRED)
+      .set_help("path to file to compile");
+
+    const auto args = parser.parse_args();
+    const std::string file = args.at("file").as<std::string>();
+
     return 0;
 }
