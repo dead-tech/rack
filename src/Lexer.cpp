8c8a9669a6b80525bef96f7b3586a3dc8c12404d
dead-tech <dev.dead.tech@gmail.com>
2023-03-25 15:03:20 +0100

    :sparkles: Lexer: Outline the basics of Lexer's API
    The Lexer is not even in a functional state. As a matter of fact
    it will practically lex nothing, as the core of its functionality,
    `Lexer::next()` is basically unimplemented.

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,1 +1,117 @@
 #include "Lexer.hpp"
+
+auto Span::create(const std::size_t start, const std::size_t end) -> Span {
+    return { start, end };
+}
+
+auto Span::start() const -> std::size_t { return this->m_start; }
+
+auto Span::end() const -> std::size_t { return this->m_end; }
+
+Span::Span(const std::size_t start, const std::size_t end)
+  : m_start{ start },
+    m_end{ end } {}
+
+auto Token::create(
+  const std::string& lexeme,
+  const TokenType    type,
+  const Span&        span
+) -> Token {
+    return { lexeme, type, span };
+}
+
+auto Token::lexeme() const -> std::string { return this->m_lexeme; }
+
+auto Token::type() const -> TokenType { return this->m_type; }
+
+auto Token::span() const -> Span { return this->m_span; }
+
+auto Token::type_to_string() const -> std::string {
+    static_assert(
+      std::to_underlying(TokenType::Max) == 2,
+      "[INTERNAL ERROR] Token::type_to_string(): Exhaustive handling of all "
+      "enum "
+      "variants is required"
+    );
+
+    switch (this->m_type) {
+        case TokenType::Number:
+            return "Number";
+        case TokenType::Plus:
+            return "Plus";
+        default: {
+            return "Unknown Token Type";
+        }
+    }
+}
+
+Token::Token(std::string lexeme, const TokenType type, const Span& span)
+  : m_lexeme{ std::move(lexeme) },
+    m_type{ type },
+    m_span{ span } {}
+
+auto Lexer::lex(const std::string& source)
+  -> std::expected<std::vector<Token>, LexError> {
+    if (source.empty()) { return std::unexpected(LexError::EmptySource); }
+
+    std::vector<Token> tokens;
+    Lexer              lexer(source);
+
+    auto&& token = lexer.next();
+    while (token.error() != LexError::Eof) {
+        if (!token.has_value()) { return std::unexpected(LexError::Eof); }
+
+        tokens.push_back(token.value());
+        token = lexer.next();
+    }
+
+    return tokens;
+}
+
+Lexer::Lexer(std::string source)
+  : m_source{ std::move(source) },
+    m_cursor{ 0 } {}
+
+auto Lexer::eof() const -> bool {
+    return this->m_cursor >= this->m_source.size();
+}
+
+auto Lexer::peek() const -> std::expected<char, LexError> {
+    if (this->eof()) { return std::unexpected(LexError::Eof); }
+    return this->m_source[this->m_cursor];
+}
+
+auto Lexer::peek_ahead(const std::size_t offset) const
+  -> std::expected<char, LexError> {
+    if (this->m_cursor + offset >= this->m_source.size()) {
+        return std::unexpected(LexError::Eof);
+    }
+    return this->m_source[m_cursor];
+}
+
+auto Lexer::next() -> std::expected<Token, LexError> {
+    while (true) {
+        const auto ch = this->peek();
+
+        if (ch.has_value() && (bool)std::isspace(ch.value())) {
+            ++this->m_cursor;
+        } else if (!ch.has_value()) {
+            return std::unexpected(ch.error());
+        } else {
+            break;
+        }
+    }
+
+    const auto current_char = this->peek();
+    if (!current_char.has_value()) { return std::unexpected(LexError::Eof); }
+
+    static_assert(
+      std::to_underlying(TokenType::Max) == 2,
+      "[INTERNAL ERROR] Lexer::next(): Exhaustive handling of all enum "
+      "variants is required"
+    );
+
+    switch (current_char.value()) {}
+
+    return Token::create("2", TokenType::Number, Span::create(0, 1));
+}
--- src/Lexer.hpp
+++ src/Lexer.hpp
@@ -1,4 +1,66 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+#include <cstdint>
+#include <expected>
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class LexError : std::uint64_t { Eof = 0, EmptySource, Max };
+
+class Span {
+  public:
+    [[nodiscard]] static auto
+      create(const std::size_t start, const std::size_t end) -> Span;
+    [[nodiscard]] auto start() const -> std::size_t;
+    [[nodiscard]] auto end() const -> std::size_t;
+
+  private:
+    Span(const std::size_t start, const std::size_t end);
+
+    std::size_t m_start;
+    std::size_t m_end;
+};
+
+enum class TokenType : std::uint64_t { Number = 0, Plus, Max };
+
+class Token {
+  public:
+    [[nodiscard]] static auto
+      create(const std::string& lexeme, const TokenType type, const Span& span)
+        -> Token;
+
+    [[nodiscard]] auto lexeme() const -> std::string;
+    [[nodiscard]] auto type() const -> TokenType;
+    [[nodiscard]] auto span() const -> Span;
+
+    [[nodiscard]] auto type_to_string() const -> std::string;
+
+  private:
+    Token(std::string lexeme, const TokenType type, const Span& span);
+
+    std::string m_lexeme;
+    TokenType   m_type;
+    Span        m_span;
+};
+
+class Lexer {
+  public:
+    [[nodiscard]] static auto lex(const std::string& source)
+      -> std::expected<std::vector<Token>, LexError>;
+
+  private:
+    explicit Lexer(std::string source);
+
+    [[nodiscard]] auto eof() const -> bool;
+    [[nodiscard]] auto peek() const -> std::expected<char, LexError>;
+    [[nodiscard]] auto peek_ahead(const std::size_t offset) const
+      -> std::expected<char, LexError>;
+    [[nodiscard]] auto next() -> std::expected<Token, LexError>;
+
+    std::string m_source;
+    std::size_t m_cursor;
+};
+
 #endif // LEXER_HPP
